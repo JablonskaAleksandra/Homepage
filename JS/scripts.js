df3bd1fa7212dd1ec9e8cf6e712871cfce811ec0
Aleksandra Jablonska <jablonska.aleksandra96@gmail.com>
2022-12-08 16:52:25 +0100

    renamed classes

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,26 +1,26 @@
 console.log("Hejka!");
 
 let selfie = document.querySelector(".selfie");
-let selfie__button = document.querySelector(".js-selfieButton");
-let selfie__CaptionChange = document.querySelector(".js-CaptionChange");
+let selfieButton = document.querySelector(".js-selfieButton");
+let selfieCaptionChange = document.querySelector(".js-CaptionChange");
 
 
-selfie__button.addEventListener("click", () => {
+selfieButton.addEventListener("click", () => {
     selfie.classList.toggle("imageHide");
     if (selfie.classList.contains("imageHide")) {
-        selfie__CaptionChange.innerText = "Pokaż zdjęcie";
+        selfieCaptionChange.innerText = "Pokaż zdjęcie";
     }
     else {
-        selfie__CaptionChange = "Ukryj zdjęcie";
+        selfieCaptionChange = "Ukryj zdjęcie";
     }
 });
 
-let theme__button = document.querySelector(".js-themeButton")
+let themeButton = document.querySelector(".js-themeButton")
 let page = document.querySelector(".page");
-let theme__name = document.querySelector(".js-themeName");
+let themeName = document.querySelector(".js-themeName");
 
-theme__button.addEventListener("click", () => {
+themeButton.addEventListener("click", () => {
     page.classList.toggle("pink");
 
-    theme__name.innerText = page.classList.contains("pink") ? "niebieski" : "różowy";
+    themeName.innerText = page.classList.contains("pink") ? "niebieski" : "różowy";
 });
